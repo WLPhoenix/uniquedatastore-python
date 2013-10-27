ec5e4c79a1ce768c9d83d1f4cd4096254ff25dc9
WLPhoenix <w.cory.hughes@gmail.com>
2013-10-27 14:26:19 -0500

    Adding everything, I'll clean up later.

---
+++ test/uniquetest.py
@@ -0,0 +1,13 @@
+import unittest
+
+class UniqueDataStoreTest( unittest.TestCase ):
+    
+    def selfTest( self ):
+        self.assertFalse( False )
+
+    
+def main():
+    unittest.main()
+
+if __name__ == '__main__':
+    main()
